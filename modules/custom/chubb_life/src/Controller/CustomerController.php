8c9df59f22bf5a47bcf8b5f184731af6c633b68e
sigmaf4f <78477179+sigmaf4f@users.noreply.github.com>
2022-10-18 19:00:37 +0800

    agent list

--- modules/custom/chubb_life/src/Controller/CustomerController.php
+++ modules/custom/chubb_life/src/Controller/CustomerController.php
@@ -65,7 +65,7 @@ class CustomerController extends ControllerBase {
     }
   }
   public static function ase_encrypt($plaintext){
-    $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+    $ivlen = openssl_cipher_iv_length($cipher="AES-256-CBC");
     $iv = openssl_random_pseudo_bytes($ivlen);
     $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
     $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
@@ -74,7 +74,7 @@ class CustomerController extends ControllerBase {
   }
   public static function ase_decrypt($plaintext){
     $c = base64_decode($ciphertext);
-    $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+    $ivlen = openssl_cipher_iv_length($cipher="AES-256-CBC");
     $iv = substr($c, 0, $ivlen);
     $hmac = substr($c, $ivlen, $sha2len=32);
     $ciphertext_raw = substr($c, $ivlen+$sha2len);
