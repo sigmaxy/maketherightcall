045dedc6258c994d15d1104cf318184ea2e1be25
sigmaxy <sigmaxy@gmail.com>
2024-04-26 13:12:18 +0800

    Bug Fix

--- modules/custom/chubb_life/src/Form/EditCallForm.php
+++ modules/custom/chubb_life/src/Form/EditCallForm.php
@@ -479,12 +479,12 @@ class EditCallForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    foreach ($form_state->getValues() as $key => $value) {
-      // @TODO: Validate fields.
-    }
-    // parent::validateForm($form, $form_state);
-  }
+  // public function validateForm(array &$form, FormStateInterface $form_state) {
+  //   // foreach ($form_state->getValues() as $key => $value) {
+  //   //   // @TODO: Validate fields.
+  //   // }
+  //   // parent::validateForm($form, $form_state);
+  // }
   /**
    * {@inheritdoc}
    */
